dda1176facf6a01455af7838a7562f51cf1a7944
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-22 13:41:42 +0000

    Updates

---
+++ precache-manifest.4efbbd28601aa03155ef4ce3bc2f551f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290714011b22de37e8a34fe73f0733a4",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "512118284608ff9a1215",
+    "url": "/to-do-list/static/css/main.5f574a7b.chunk.css"
+  },
+  {
+    "revision": "9bb1479f395541321cc7",
+    "url": "/to-do-list/static/js/2.f22a70f1.chunk.js"
+  },
+  {
+    "revision": "512118284608ff9a1215",
+    "url": "/to-do-list/static/js/main.a552b1ae.chunk.js"
+  },
+  {
+    "revision": "9e03dd881c8e1fd213e0",
+    "url": "/to-do-list/static/js/runtime-main.5ae6368c.js"
+  }
+]);
